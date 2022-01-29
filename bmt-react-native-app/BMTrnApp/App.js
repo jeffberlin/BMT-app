15432619a45207fcc77fd31448e13f6f7743f5ef
Jeff Berlin <jeff@jbrally.com>
2022-01-28 20:39:24 -0500

    Adding support for device appearance

--- bmt-react-native-app/BMTrnApp/App.js
+++ bmt-react-native-app/BMTrnApp/App.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Appearance, AppearanceProvider, useColorScheme } from 'react-native-appearance';
 import Login from './components/Login';
 
-const App = () => {
+export default function AppContainer() {
   return (
-    <View style={{flex: 1}}>
-      <Login />
-      <StatusBar style="auto" />
+    <AppearanceProvider>
+      <App />
+    </AppearanceProvider>
+  );
+}
+
+function App() {
+  const colorScheme = useColorScheme();
+
+  const themeStatusBarStyle = colorScheme === 'light' ? 'dark-content' : 'light-content';
+  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
+  return (
+    <View style={[styles.container, themeContainerStyle]}>
+      <Login style={[themeTextStyle, themeContainerStyle]} />
+
+      <StatusBar barStyle={themeStatusBarStyle} />
     </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  lightContainer: {
+    backgroundColor: '#fff'
+  },
+  darkContainer: {
+    backgroundColor: '#1c1e21'
+  },
+  lightThemeText: {
+    color: '#707070'
+  },
+  darkThemeText: {
+    color: '#545454'
+  },
+});
+
+// export default App;
--- bmt-react-native-app/BMTrnApp/components/Login.js
+++ bmt-react-native-app/BMTrnApp/components/Login.js
@@ -1,59 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Dimensions, TextInput, Pressable, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, TextInput, Pressable, TouchableOpacity, Linking } from 'react-native';
 
 const width = Dimensions.get('window').width;
 
 const Login = () => {
-  const [text, onChangeText] = React.useState("");
-  const [number, onChangeNumber] = React.useState(null);
+  const [username, onChangeUsername] = React.useState("");
+  const [password, onChangePassword] = React.useState(null);
+  const getYear = () => {
+		return new Date().getFullYear()
+	}
   return (
-    <View style={styles.container}>
-      <Image source={require("../assets/bmt-micro-nav-logo-min.png")} style={styles.image} resizeMode={'contain'}/>
+    <View style={loginStyles.loginContainer}>
+      <Image source={require("../assets/bmt-micro-nav-logo-min.png")} style={loginStyles.image} resizeMode={'contain'} />
+      {/* <Image source={require("../assets/bmt-micro-logo-new-white.png")} style={styles.image} resizeMode={'contain'} /> */}
       <View>
-        <Text style={styles.inputText}>Login:</Text>
+        <Text style={loginStyles.inputText}>Login:</Text>
         <TextInput
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
+          style={loginStyles.input}
+          onChangeText={onChangeUsername}
+          value={username}
+          textContentType='username'
+          placeholder='Vendor ID'
+          // placeholderTextColor='#545454'
         />
-        <Text style={styles.inputText}>Password:</Text>
+        <Text style={loginStyles.inputText}>Password:</Text>
         <TextInput
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
+          style={loginStyles.input}
+          onChangeText={onChangePassword}
+          value={password}
+          textContentType='password'
+          placeholder='Password'
+          // placeholderTextColor='#545454'
         />
         <Pressable>
-          <Text style={{ marginBottom: 30, letterSpacing: .5 }}>Forgot Password?</Text>
+          <Text
+            style={{ marginBottom: 30, letterSpacing: .5, color: '#195a7c', paddingVertical: 2, paddingHorizontal: 3 }}
+            onPress={() => Linking.openURL('https://vendors.bmtmicro.com/login-lost-password.jsp')}
+          >
+            Forgot Password?
+          </Text>
         </Pressable>
       </View>
       <View>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>Login</Text>
+        <TouchableOpacity style={[ loginStyles.button, {marginBottom: 250} ]}>
+          <Text style={loginStyles.text}>Login</Text>
         </TouchableOpacity>
       </View>
+      <Text style={{ letterSpacing: .5 }}>&copy;&nbsp;BMT Micro, Inc. 1992 - {getYear()}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const loginStyles = StyleSheet.create({
+  loginContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   image: {
     width: width * .3,
-    marginTop: -100
+    marginTop: 50,
   },
   input: {
-    height: 40,
+    height: 45,
     marginTop: 5,
     marginBottom: 15,
     borderWidth: 1,
     padding: 10,
-    width: 250,
+    width: 300,
     borderRadius: 4,
+    borderColor: '#195a7c',
+    color: '#fff',
+    letterSpacing: .75
   },
   button: {
     backgroundColor: '#195a7c',
@@ -71,7 +89,11 @@ const styles = StyleSheet.create({
   inputText: {
     fontSize: 18,
     letterSpacing: .5,
-    color: '#707070'
+    // color: '#777777'
+  },
+  forgotText: {
+    color: '#195a7c',
+
   }
 });
 
